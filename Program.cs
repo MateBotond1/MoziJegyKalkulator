88c805e0e62abd1b0cc9f56d71584a1b1ddddb7f
MateBotond1 <mate.botond.2024d@telekimezotur.hu>
2026-09-14 13:44:26 +0200

    2. Feladat

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Írja be a nevét:");
+string VNev = Console.ReadLine();
+Console.WriteLine("Írja be az életkorát:");
+int Életkor = int.Parse(Console.ReadLine());
+Console.WriteLine("van diákod?('Igen' v. 'Nem' )");
+string Diak = Console.ReadLine();
+Console.WriteLine("Mozijegy ára:");
+int Mozijegyar = int.Parse(Console.ReadLine());
+Console.WriteLine("Popcorn db");
+int PopcornDB = int.Parse(Console.ReadLine());
+Console.WriteLine("Popcorn ára");
+int PopcornAR = int.Parse(Console.ReadLine());
+Console.WriteLine("Kedvezmény");
+double Kedvezmeny = double.Parse(Console.ReadLine());
